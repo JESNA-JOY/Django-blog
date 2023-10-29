ea86a643a97ee466633d804cb76a435650328663
JESNA-JOY <joyjesna18052001@gmail.com>
2023-10-30 00:01:03 +0530

    first commit

---
+++ ablog/theblog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+# def home(request):
+ #   return render(request, 'home.html', {})
+
+class HomeView(ListView):
+  model= Post
+  template_name='home.html'
