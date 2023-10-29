229eae11898c8b0cac852f27d4aa89a7553971ae
JESNA-JOY <joyjesna18052001@gmail.com>
2023-10-30 01:17:18 +0530

    just a django page with html file and created the post in the admin area

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-# def home(request):
- #   return render(request, 'home.html', {})
 
-class HomeView(ListView):
-  model= Post
-  template_name='home.html'
+def home(request):
+   return render(request, 'home.html', {})
+
+
