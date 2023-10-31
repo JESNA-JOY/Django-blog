a86b4f2092f6b47ce4eae2931bbeba055230a8f7
JESNA-JOY <joyjesna18052001@gmail.com>
2023-11-01 02:00:21 +0530

    Added blogPost to django Webpage

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .import views
+#from .import views
+from .views import HomeView,Article_DetailView
 urlpatterns = [
-path('',views.home, name='home')
+#path('',views.home, name='home')
+path('', HomeView.as_view(), name = "home" ),
+path('article/<int:pk>', Article_DetailView.as_view(), name= "articledetail" ),
 ]
